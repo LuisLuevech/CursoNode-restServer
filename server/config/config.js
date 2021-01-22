ed25bba6a732f8e1a94a80a3368ff630f61c9d29
Luis Borrego <luevech@gmail.com>
2021-01-22 10:00:03 +0100

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,25 @@
 // ==========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//=======================
+//     ENTORNO
+//=======================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=======================
+//     BASE DE DATOS
+//=======================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+} else {
+    urlDB = 'mongodb+srv://luevech:Lu25172517@cluster0.b3eww.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB
